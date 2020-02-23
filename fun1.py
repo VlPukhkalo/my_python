db6996ffb3b29ed0211bedcbfe21291446c1fb42
Vlada Pukhkalo <suzdareva666@yandex.ru>
2020-02-23 23:22:54 +0300

    init commit

---
+++ fun1.py
@@ -0,0 +1,5 @@
+x=float(input("Введите x: "))
+if -2.4<=x<=5.7:
+    print(x**2)
+else:
+    print(x)
---
+++ get_parity.py
@@ -0,0 +1,5 @@
+def parity(a):
+    if a%2==0:
+        return 'Четное'
+    else:
+        return 'Нечетное'
